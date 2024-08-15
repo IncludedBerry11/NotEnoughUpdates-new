7541bb0a877bee66c2c84cae65594bd6067550db
IncludedBerry11 <116087837+IncludedBerry11@users.noreply.github.com>
2024-08-15 14:04:43 +0530

    Update TablistAPI.kt

--- src/main/kotlin/io/github/moulberry/notenoughupdates/miscfeatures/tablisttutorial/TablistAPI.kt
+++ src/main/kotlin/io/github/moulberry/notenoughupdates/miscfeatures/tablisttutorial/TablistAPI.kt
@@ -35,9 +35,6 @@ object TablistAPI {
 
     @SubscribeEvent
     fun onWorldSwitch(event: EntityJoinWorldEvent) {
-        if (event.entity == Minecraft.getMinecraft().thePlayer) {
-            lastWorldSwitch = System.nanoTime()
-        }
     }
 
     @JvmStatic
@@ -46,43 +43,7 @@ object TablistAPI {
         addToQueue: Boolean,
         showNotification: Boolean
     ): List<String> {
-        val regex = widget.widgetName.regex ?: Regex.fromLiteral("${widget.widgetName}:")
-        val list = mutableListOf<String>()
-        // If not a single reset is present, the tab list hasn't been initialized yet
-        var sawReset = false
-
-        for (entry in TabListUtils.getTabList()) {
-            if (entry.contains("§r")) {
-                sawReset = true
-            }
-
-            if (list.isNotEmpty()) {
-                // Empty line
-                // Or there is no spacing between two widgets
-                // The widget ends here.
-                if (entry == "§r" || entry.startsWith("§r§")) {
-                    break
-                }
-
-                // New tab column, ignore it and continue with the same widget
-                if (entry == "§r               §r§3§lInfo§r") {
-                    continue
-                }
-
-                list.add(entry)
-            } else if (entry.stripControlCodes().matches(regex)) {
-                list.add(entry)
-            }
-        }
-        if (addToQueue &&
-            list.isEmpty() &&
-            sawReset &&
-            (System.nanoTime() - lastWorldSwitch > 10_000_000_000L) &&
-            widget.widgetName != lastWidgetEnabled
-        ) {
-            TablistTaskQueue.addToQueue(widget, showNotification)
-        }
-        return list
+        return null
     }
 
     /**
@@ -91,11 +52,7 @@ object TablistAPI {
      */
     @JvmStatic
     fun getWidgetLines(widgetName: WidgetNames): List<String> {
-        return getWidgetLinesInRegion(
-            TablistTutorial.TabListWidget("CURRENT_REGION", widgetName),
-            addToQueue = true,
-            showNotification = true
-        )
+        return null
     }
 
     /**
@@ -106,11 +63,7 @@ object TablistAPI {
      */
     @JvmStatic
     fun getWidgetLinesWithoutNotification(widgetName: WidgetNames): List<String> {
-        return getWidgetLinesInRegion(
-            TablistTutorial.TabListWidget("CURRENT_REGION", widgetName),
-            addToQueue = true,
-            showNotification = false
-        )
+        return null
     }
 
     /**
@@ -121,48 +74,9 @@ object TablistAPI {
      */
     @JvmStatic
     fun getOptionalWidgetLines(widgetName: WidgetNames): List<String> {
-        return getWidgetLinesInRegion(
-            TablistTutorial.TabListWidget("CURRENT_REGION", widgetName),
-            addToQueue = false,
-            showNotification = false
-        )
+        return null
     }
 
     enum class WidgetNames(val regex: Regex?) {
-        COMMISSIONS(null),
-        /*
-            '§e§lSkills:'
-            ' Farming 50: §r§a43.3%'
-            ' Mining 60: §r§c§lMAX'
-            ' Combat 46: §r§a21.7%'
-            ' Foraging 23: §r§a43.5%'
-        * */
-        SKILLS(Regex("Skills:( .*)?")),
-        /*
-        * '§e§lSkills: §r§aCombat 46: §r§321.7%'
-        * */
-        DUNGEON_SKILLS(Regex("Skills: (.*)")),
-        TRAPPER(null),
-        FORGE(Regex("Forges:( \\(\\d/\\d\\))?")),
-        POWDER(Regex.fromLiteral("Powders:")),
-        PROFILE(Regex("Profile: ([A-Za-z]+)( .*)?")),
-        ACTIVE_EFFECTS(Regex("Active Effects(: \\(\\d+\\))?")),
-        COOKIE_BUFF(Regex("Cookie Buff")),
-        PET(null),
-        ;
-
-        override fun toString(): String {
-            return this.name.lowercase().split("_").joinToString(" ") { str ->
-                str.replaceFirstChar {
-                    if (it.isLowerCase()) {
-                        it.titlecase(
-                            Locale.ROOT
-                        )
-                    } else {
-                        it.toString()
-                    }
-                }
-            }
-        }
     }
 }
